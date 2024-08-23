32e40b38a27d8cc194ecb4073576ea25892731d8
Anest <efremenko.ira.rzn@gmail.com>
2024-08-23 23:46:15 +0300

    Выполнено 2-е задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -18,3 +18,44 @@ function solveEquation(a, b, c) {
 
   return arr;
 }
+
+
+
+"use strict";
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+
+  let p = parseInt(percent) / 100 / 12// процентная ставка
+  let cntrb = parseInt(contribution) // первоначальный взнос
+  let  amnt = parseInt(amount) // сумма кредита
+  let nm = parseInt(countMonths) // количество месяцев
+
+  if (isNaN(p) || p < 0) {
+    return `Параметр "Процентная ставка" - введено неверное значение "$(persent)"`;
+  }
+  else if (isNaN(cntrb) || cntrb < 0) {
+    return `Параметр "Первоначальный взнос" - введено неверное значение "$(contribution)"`;
+  }
+  else if (isNaN(amnt) || amnt < 0) {
+    return `Параметр "Сумма кредита" - введено неверное значение "$(amount)"`;
+  }
+  else if (isNaN(nm) || nm < 0) {
+    return `Параметр "Количество месяцев" - введено неверное значение "$(countMonths)"`;
+  }
+
+  else {
+    
+  
+  let s = amnt - cntrb; // тело кредита - сумма которую нужно вернуть банку//
+
+ let monthlyPay = s * (p + (p / (((1 + p)**nm) - 1))) // ежемесячная оплата //
+  let totalPay = (monthlyPay * nm).toFixed(2);
+
+
+  
+  console.log(calculateTotalMortgage(percent, contribution, amount, countMonths));
+
+    return totalPay
+
+}
+}
